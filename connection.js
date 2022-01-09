f07254279371e987cf1a825bc665b326a73ea24d
VladShkliaruk <masterlordgoker@gmail.com>
2022-01-09 14:41:50 +0200

    create operation for db

--- connection.js
+++ connection.js
@@ -3,13 +3,15 @@ const { MongoClient } = require('mongodb');
 const listDatabases = require('./listDBs')
 const password = process.env.PASS
 const createListing = require('./createList')
+const createMultyListing = require('./manyListings')
 const main = async () => {
   const uri = `mongodb+srv://masterlordgoker:${password}@cluster0.ekxqw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
   const client = new MongoClient(uri);
   try {
     await client.connect();
     // await listDatabases(client);
-    await createListing(client, { firstName: 'Vlad', secondName: 'Shkliaruk' })
+    // await createListing(client, { firstName: 'Vlad', secondName: 'Shkliaruk' })
+    await createMultyListing(client, [{name:'Kolia'},{name: 'Sveta'}])
   } catch (err) {
     console.error(err);
   } finally {
