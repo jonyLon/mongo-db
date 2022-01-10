61a908fbcfe282373ca4e6f8097d06b50dfb08d5
VladShkliaruk <masterlordgoker@gmail.com>
2022-01-10 18:01:50 +0200

    improve log

--- CRUD/updateOneObj.js
+++ CRUD/updateOneObj.js
@@ -1,8 +1,8 @@
 const updateOneObj = async (client,{dbName, collectionName}, filterbyName, updatedObj) => {
   const result = await client.db(dbName).collection(collectionName)
   .updateOne({name: filterbyName}, {$set: updatedObj});
-  console.log(`Object(s) matched: ${result.matchedCount}`)
-  console.log(`Object(s) updated: ${result.modifiedCount}`)
+  console.log(`Object(s) was/were matched: ${result.matchedCount}`)
+  console.log(`Object(s) was/were updated: ${result.modifiedCount}`)
   console.log(`Result: `)
   console.dir(result)
 }
